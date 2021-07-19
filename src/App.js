600905af2c2bd37ef75335de62bdb8d307255a12
Saumyasmith Muni <ssm07121994@gmail.com>
2021-07-19 22:19:28 +0530

    adding project base

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./Components/ExpenseItem";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2020, 4, 28),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: 799.99,
+      date: new Date(2020, 9, 6),
+    },
+    {
+      id: "e4",
+      title: "New Wooden Desk",
+      amount: 450,
+      date: new Date(2020, 5, 12),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
